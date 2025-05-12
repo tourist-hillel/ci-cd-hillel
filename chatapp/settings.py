982a4911745b1679d73c48c716b247ada4d6c4a7
Inferato <infaustmager@gmail.com>
2025-05-12 20:31:51 +0300

    deploy setup: all hosts allowed

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q#c#caqrd&t4x$=$nk21x!2rapdm9#mv(=hfpz2bg!9au=cp%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hillel-deploy.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
